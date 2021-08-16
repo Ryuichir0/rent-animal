7efa355b8662df9f3bdc9d509f2fcee1c0689ec4
aurelien canonne <ryuichiro@outlook.fr>
2021-08-16 14:51:11 +0200

    Validation & Association

--- app/models/herd.rb
+++ app/models/herd.rb
@@ -1,3 +1,7 @@
 class Herd < ApplicationRecord
   belongs_to :user
+
+  validates :unit_price, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true, length: { minimum: 20 }
 end
