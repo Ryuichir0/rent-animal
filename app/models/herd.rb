3d7bca0798eb5433a1558bf2c7b818c0066d1550
cedric ettori <ettori.cedric@gmail.com>
2021-08-18 16:15:28 +0200

    validation address + validation address controller

--- app/models/herd.rb
+++ app/models/herd.rb
@@ -4,7 +4,8 @@ class Herd < ApplicationRecord
   validates :unit_price, presence: true
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true, length: { minimum: 20 }
-
+  validates :address, presence: true
+  
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
