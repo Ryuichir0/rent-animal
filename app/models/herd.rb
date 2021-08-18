4025f344e26eb3b5f97859caf225c3fe67e26351
cedric ettori <ettori.cedric@gmail.com>
2021-08-18 16:16:40 +0200

    reduction validation description

--- app/models/herd.rb
+++ app/models/herd.rb
@@ -3,9 +3,9 @@ class Herd < ApplicationRecord
 
   validates :unit_price, presence: true
   validates :name, presence: true, uniqueness: true
-  validates :description, presence: true, length: { minimum: 20 }
+  validates :description, presence: true, length: { minimum: 6 }
   validates :address, presence: true
-  
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
