706ef41b153760d11ba461e24604eecfd4081eaf
cedric ettori <ettori.cedric@gmail.com>
2021-08-19 14:24:32 +0200

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root to: 'pages#home'
   resources :herds do
     resources :bookings
   end
+  get 'dashboards/index'
   resources :bookings, only: [:destroy, :edit]
   resources :herds, only: [:destroy, :edit]
   # get "/dashboard", to: "herds#index", as: :herds
