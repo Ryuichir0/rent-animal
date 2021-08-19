d36b5c896d21d15b9fc0ed2f1bd5f8c49061b83f
Raphael Aubry <raphael.aubry@hotmail.fr>
2021-08-19 17:47:16 +0200

    yaya calendar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,12 @@ class ApplicationController < ActionController::Base
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resources)
+  private
+
+  def after_sign_in_path_for()
     herds_path
   end
+
   protected
 
   def update_allowed_parameters
