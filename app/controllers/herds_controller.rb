174586000ebd6c29e10a7a18535dbc0da88270dc
aurelien canonne <ryuichiro@outlook.fr>
2021-08-20 09:05:13 +0200

    problem solved

--- app/controllers/herds_controller.rb
+++ app/controllers/herds_controller.rb
@@ -1,35 +1,17 @@
 class HerdsController < ApplicationController
   before_action :authenticate_user!
-
-<<<<<<< HEAD
-    def index
-      # @herds = Herd.where(user_id: current_user)
-      if params[:query]
-        @herds = Herd.search_by_name_and_address(params[:query])
-      else
-        @herds = Herd.all
-      end
-      @markers = @herds.geocoded.map do |herd|
-        {
-          lat: herd.latitude,
-          lng: herd.longitude,
-          info_window: render_to_string(partial: "info_windows", locals: { herd: herd })
-        }
-      end
-=======
-    if params[:query] && params[:query] != ""
-      @herds = Herd.search_by_name_and_address(params[:query])
-    else
-      @herds = Herd.all
-    end
-    @markers = @herds.geocoded.map do |herd|
-      {
-        lat: herd.latitude,
-        lng: herd.longitude,
-        info_window: render_to_string(partial: "info_windows", locals: { herd: herd })
-      }
->>>>>>> 8b87a6291b1494a7b0a7e66382c4af1651f31ed7
-    end
+  if params[:query] && params[:query] != ""
+    @herds = Herd.search_by_name_and_address(params[:query])
+  else
+    @herds = Herd.all
+  end
+  @markers = @herds.geocoded.map do |herd|
+    {
+      lat: herd.latitude,
+      lng: herd.longitude,
+      info_window: render_to_string(partial: "info_windows", locals: { herd: herd })
+    }
+  end
 
   def show
     @herd = Herd.find(params[:id])
